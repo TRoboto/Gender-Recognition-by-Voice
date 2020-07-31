e10e8d09a2b021298563ba03a3f21af470aec6aa
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-07-31 03:13:21 +0300

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,7 +5,8 @@ import os
 import librosa
 import pandas as pd
 import tqdm
-from utils import extract_features
+import pickle
+from utils import extract_features, extract_2d_features
 
 label2int = {
     "male": 1,
@@ -26,7 +27,7 @@ def plot_raw_audio(audio_file):
     plt.show()
 
 
-def load_data(data_path='../dataset/cv-valid-train_filtered.csv', vector_length=187):
+def load_data_1d(data_path='../dataset/cv-valid-train_filtered.csv', vector_length=187):
     """A function to load gender recognition dataset from `dataset` folder
     After the second run, this will load from results/features.npy and results/labels.npy files
     as it is much faster!"""
@@ -62,4 +63,41 @@ def load_data(data_path='../dataset/cv-valid-train_filtered.csv', vector_length=
     # so we won't load each one of them next run
     np.save("results/features", X)
     np.save("results/labels", y)
+    return X, y
+
+def load_data_2d(data_path='../dataset/cv-valid-train_filtered.csv'):
+    """A function to load gender recognition dataset from `dataset` folder
+    After the second run, this will load from results/features2d.pkl and results/labels.pkl files
+    as it is much faster!"""
+    # make sure results folder exists
+    if not os.path.isdir("results"):
+        os.mkdir("results")
+    # if features & labels already loaded individually and bundled, load them from there instead
+    if os.path.isfile("results/features2d.pkl") and os.path.isfile("results/labels2d.pkl"):
+        X = pickle.load("results/features2d.pkl", allow_pickle=True)
+        y = pickle.load("results/labels2d.pkl")
+        return X, y
+    # read dataframe
+    df = pd.read_csv(data_path)
+    # get total samples
+    n_samples = len(df)
+    # get total male samples
+    n_male_samples = len(df[df['gender'] == 'male'])
+    # get total female samples
+    n_female_samples = len(df[df['gender'] == 'female'])
+    print("Total samples:", n_samples)
+    print("Total male samples:", n_male_samples)
+    print("Total female samples:", n_female_samples)
+    # initialize an empty array for all audio features
+    X = [np.array([]) for _ in range(n_samples)]
+    # initialize an empty array for all audio labels (1 for male and 0 for female)
+    y = np.zeros((n_samples, 1))
+    for i, (filename, gender) in tqdm.tqdm(enumerate(zip(df['filename'], df['gender'])), "Loading data", total=n_samples):
+        filename = '../dataset/' + filename
+        X[i] = extract_2d_features(filename, mel=True) 
+        y[i] = label2int[gender]
+    # save the audio features and labels into files
+    # so we won't load each one of them next run
+    np.save("results/features2d", X)
+    np.save("results/labels2d", y)
     return X, y
