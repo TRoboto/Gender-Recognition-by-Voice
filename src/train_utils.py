252eb9a46393668146e9d1c4f81ae5ba7185a860
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-07-31 20:19:26 +0300

    Update train_utils.py

--- src/train_utils.py
+++ src/train_utils.py
@@ -1,32 +1,101 @@
 import math
 import sys
 import time
+from tqdm import tqdm
 import torch
+import torch.optim as optim
+import torch.nn as nn
 import numpy as np
+import copy
+import config
 
-def train_model(model, optimizer, data_loader, device, criterion, epochs = 10, print_freq = 1):
-    model.train()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    model.to(device)
-    for ep in range(1, epochs + 1):
 
-        running_loss = 0.0
-        for features, targets in data_loader:
-            features = list(feat.to(device) for feat in features)
-            targets = targets.to(device)
-            
-            outs = model(features)
+def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name, num_epochs=config.EPOCHS):
+    since = time.time()
 
-            loss = criterion(outs, targets)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-            running_loss += loss.item()
-            # print loss statistics
-            if ep % print_freq == 0:
-                print('Epoch: {:>4}/{:<4} Avg. Loss: {}\n'.format(
-                    ep, epochs, np.average(running_loss)))
-                running_loss = 0
-                
-    print('Finished Training')
+    for epoch in range(1, num_epochs + 1):
+        print('Epoch {}/{}'.format(epoch, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in tqdm(dataloaders[phase], total=len(dataloader.dataset[phase])):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / len(dataloader.dataset[phase])
+            epoch_acc = running_corrects.double() / len(dataloader.dataset[phase])
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # save best model weights
+    torch.save(best_model_wts, 'results/' + model_name)
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+def eval_fn(model, data_loader):
+    ''' Evaluate model on the test set 
+    '''
+    model.eval()
+    counter = 0
+    running_corrects = 0
+    with torch.no_grad():
+        for bi, (inputs, labels) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            counter += len(labels)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            running_corrects += torch.sum(preds == labels.data)
+
+    print('Test set ACC:', running_corrects / counter)
