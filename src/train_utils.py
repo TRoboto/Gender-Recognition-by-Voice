24d802d612f294fa789e04e8075986e896909cf0
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-08-01 03:12:56 +0300

    Update train_utils.py

--- src/train_utils.py
+++ src/train_utils.py
@@ -9,7 +9,7 @@ import numpy as np
 import copy
 import config
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name, num_epochs=config.EPOCHS):
@@ -17,7 +17,6 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name,
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-
     for epoch in range(1, num_epochs + 1):
         print('Epoch {}/{}'.format(epoch, num_epochs))
         print('-' * 10)
@@ -32,10 +31,19 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name,
             running_loss = 0.0
             running_corrects = 0
 
+            # switch to cuda if available
+            model.to(device)
+
+            batch_size = config.TRAIN_BATCH_SIZE if phase == 'train' else config.VALID_BATCH_SIZE
+            datalength = len(dataloaders[phase].dataset)
+            total_length =  datalength // batch_size
+
+            counter = 0
             # Iterate over data.
-            for inputs, labels in tqdm(dataloaders[phase], total=len(dataloader.dataset[phase])):
+            for inputs, labels in tqdm(dataloaders[phase], total=total_length):
                 inputs = inputs.to(device)
-                labels = labels.to(device)
+                labels = labels.to(device, dtype=torch.float)
+                # labels = labels.type(torch.FloatTensor)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -44,7 +52,6 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name,
                 # track history if only in train
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
                     # backward + optimize only if in training phase
@@ -54,12 +61,12 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name,
 
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                running_corrects += torch.sum(labels.view_as(outputs) == torch.round(outputs))
             if phase == 'train':
                 scheduler.step()
 
-            epoch_loss = running_loss / len(dataloader.dataset[phase])
-            epoch_acc = running_corrects.double() / len(dataloader.dataset[phase])
+            epoch_loss = running_loss / datalength
+            epoch_acc = running_corrects.double() / datalength
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
@@ -77,19 +84,19 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, model_name,
     print('Best val Acc: {:4f}'.format(best_acc))
 
     # save best model weights
-    torch.save(best_model_wts, 'results/' + model_name)
+    torch.save(best_model_wts, 'results/' +  model_name + '_acc_{:.4f}'.format(best_acc.cpu().detach().numpy()) + ".pt")
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
 
-def eval_fn(model, data_loader):
+def eval_fn(model, test_loader):
     ''' Evaluate model on the test set 
     '''
     model.eval()
     counter = 0
     running_corrects = 0
     with torch.no_grad():
-        for bi, (inputs, labels) in tqdm(enumerate(data_loader), total=len(data_loader)):
+        for bi, (inputs, labels) in tqdm(enumerate(test_loader), total=len(test_loader)):
             counter += len(labels)
             inputs = inputs.to(device)
             labels = labels.to(device)
