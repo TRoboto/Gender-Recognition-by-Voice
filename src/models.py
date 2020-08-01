320808b1d9d10d875b7ed5af2fdb5f19a68ace69
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-08-01 03:12:54 +0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -2,17 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as I
+# set seed for reproducibility
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+torch.backends.cudnn.deterministic = True
 
-
-def simple_ann(nn.Module):
+class simple_ann(nn.Module):
     def __init__(self, input_shape = 187):
         super(simple_ann, self).__init__()
 
         self.fc1 = nn.Linear(input_shape, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 128)
-        self.fc5 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
         self.out = nn.Linear(64, 1)
 
         self.dropout = nn.Dropout(0.3)
@@ -21,11 +22,12 @@ def simple_ann(nn.Module):
     
     def forward(self, x):
 
-        x = self.dropout(F.relu(self.bn1(self.fc1(x))))
-        x = self.dropout(F.relu(self.bn1(self.fc2(x))))
-        x = self.dropout(F.relu(self.bn2(self.fc3(x))))
-        x = self.dropout(F.relu(self.bn2(self.fc4(x))))
-        x = self.dropout(F.relu(self.fc5(x)))
-        out = F.sigmoid(self.out(x))
-
-        return out
+        x = F.relu(self.fc1(x))
+        # x = self.bn1(x)
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        # x = self.bn2(x)
+        x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
+        return torch.sigmoid(self.out(x))
