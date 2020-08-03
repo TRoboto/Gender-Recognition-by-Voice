53ee868bfcb1240656a1a403586b497215ca95db
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-08-04 00:58:08 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,5 @@ TRAIN_BATCH_SIZE = 20
 VALID_BATCH_SIZE = 20
 DATA_PATH = '../dataset/cv-valid-train.csv'
 TEST_DATA_PATH = '../dataset/cv-valid-test.csv'
+SCALAR_PATH = 'results/scaler.pkl'
+MODEL_PATH = 'results/final_model.model'
