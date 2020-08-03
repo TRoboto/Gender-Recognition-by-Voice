9fa9e74ade91ac29a115c8954d8c001bdfd89c25
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-08-03 04:05:51 +0300

    Semi final

--- src/main.py
+++ src/main.py
@@ -1,31 +1,15 @@
-from data_utils import *
-from train_utils import train_model
-from models import *
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
+from lazypredict.Supervised import LazyClassifier
+
 import config
-import itertools
-import threading
-import time
-import sys
+from data_utils import *
+from models import *
+from train_utils import train_model
 
 
-# for loading animation
-done = False
-
-
-def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rloading data ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rDone!     \n')
-
-
-def run_1d():
+def train_ann():
     # Load the dataset
     X, y = load_data()
     # Balance it
@@ -40,7 +24,7 @@ def run_1d():
     dataloaders['train'] = get_dataloader(X_train, y_train)
     dataloaders['val'] = get_dataloader(X_val, y_val, 'val')
     # Define models
-    net = simple_ann_model()
+    net = ann_model()
     # Define the loss fn, the optimizer and the scheduler
     criterion = nn.BCELoss()
     optimizer = optim.Adam(net.parameters(), lr=1e-3)
@@ -48,35 +32,22 @@ def run_1d():
 
     # train the model
     train_model(net, criterion, optimizer, scheduler,
-                dataloaders, 'simple_ann_model')
+                dataloaders, 'ann_model')
 
-
-def run_2d():
-    global done
-    # read dataframe
-    X, y = load_2d_data()
-    # stop loading animation
-    done = True
-    # Balance it
-    Xb, yb = balance_dataset(X, y)
+def train_lazy():
+    # Load the dataset
+    X, y = load_data()
     # Split the data
-    X_train, X_val, y_train, y_val = split_dataset(Xb, yb)
-    # Get dataloaders
-    dataloaders = {}
-    dataloaders['train'] = get_dataloader(X_train, y_train)
-    dataloaders['val'] = get_dataloader(X_val, y_val, 'val')
-    # Define models
-    net = rnn_model()
-    # Define the loss fn, the optimizer and the scheduler
-    criterion = nn.BCELoss()
-    optimizer = optim.Adam(net.parameters(), lr=1e-3)
-    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
-    # train the model
-    train_model(net, criterion, optimizer, scheduler,
-                dataloaders, 'rnn_model')
-
+    X_train, X_val, y_train, y_val = split_dataset(X, y)
+    # # Normalize
+    # X_train = normalize(X_train)
+    # X_val = normalize(X_val, False)
+    # define classifier
+    clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+    # fit
+    models,predictions = clf.fit(X_train, X_val, y_train, y_val)
+    # print
+    print(models)
 
 if __name__ == "__main__":
-    t = threading.Thread(target=animate)
-    t.start()
-    run_2d()
+    train_lazy()
